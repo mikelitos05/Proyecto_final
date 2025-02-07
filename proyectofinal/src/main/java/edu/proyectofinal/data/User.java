1cfae7307ec2f081848d454f8c74b6dec273a512
mikeess05 <miguelmendoza.05.mm@gmail.com>
2025-02-06 19:18:45 -0600

    Agregue todo el funcionamiento de Loan, LoanManager, User y UserManager

--- proyectofinal/src/main/java/edu/proyectofinal/data/User.java
+++ proyectofinal/src/main/java/edu/proyectofinal/data/User.java
@@ -1,3 +1,48 @@
+package edu.proyectofinal.data;
 public class User {
+
+    private String name;
+    private int activeLend;
+
+
+    public User(String name, int activeLend) {
+        this.name = name;
+        this.activeLend = activeLend;
+    }
+
+    public int getActiveLend() {
+        return activeLend;
+    }
+
+    public void setActiveLend(int activeLend) {
+        if (activeLend < 0 || activeLend > 3) {
+            throw new IllegalArgumentException("Numero incorrecto de prestamos");
+        }
+        else{
+            this.activeLend = activeLend;
+        }
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("Nombre es null");
+        }
+        else {
+            this.name = name;
+        }
+    }
+
+
+
+
+
+
+
+
 
 }
--- proyectofinal/src/main/java/edu/proyectofinal/process/UserManager.java
+++ proyectofinal/src/main/java/edu/proyectofinal/process/UserManager.java
@@ -1,3 +1,29 @@
+package edu.proyectofinal.process;
+
+import edu.proyectofinal.data.User;
+
+import java.util.ArrayList;
+
 public class UserManager {
+    ArrayList<User> users;
+
+    public UserManager() {
+        this.users = new ArrayList<>();
+    }
+
+    public void addUser(String name, int activeLend){
+        User user = new User(name, activeLend);
+        users.add(user);
+    }
+
+    public ArrayList<User> getUsers(){
+        return this.users;
+    }
+
+
+
+
+
+
 
 }
