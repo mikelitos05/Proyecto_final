199766a64d960893c36bfc915ba0b6ee55c6789f
Macszimo444 <maximo.agleza>
2025-02-06 22:01:33 -0600

    Añado clases de Book y BookManager

--- proyectofinal/src/main/java/edu/proyectofinal/data/Book.java
+++ proyectofinal/src/main/java/edu/proyectofinal/data/Book.java
@@ -1,4 +1,54 @@
 public class Book {
     
+    private String title;
+    private String author;
+    private String description;
+    private int totalCopies;
+    private int availableCopies;
 
+    public Book(String title, String author, String description, int totalCopies) {
+        this.title = title;
+        this.author = author;
+        this.description = description;
+        this.totalCopies = totalCopies;
+        this.availableCopies = totalCopies;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    
+    public int getTotalCopies(){
+        return totalCopies;
+    }
+
+    public int getAvailabilityCopies(){
+        return availableCopies;
+    }
+
+    public void setAvailabilityCopies(int availableCopies) {
+        if (availableCopies <= totalCopies) {
+            this.availableCopies = availableCopies;
+        } else {
+            System.out.println("ERROR: No es posible tener más copias disponibles que el total.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "BoOk{" + 
+                "title ->"  + title + 
+                ", author ->" + author + 
+                "availableCopies ->" + availableCopies + 
+                '}';
+    }
 }
+
--- proyectofinal/src/main/java/edu/proyectofinal/process/BookManager.java
+++ proyectofinal/src/main/java/edu/proyectofinal/process/BookManager.java
@@ -1,3 +1,55 @@
-public class BookManager {
 
+import java.awt.print.Book;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookManager {
+    private List<Book> books;
+
+    public BookManager() {
+        this.books = new ArrayList<>();
+        loadPreloadedBooks();
+    }
+
+    private void loadPreloadedBooks() {
+        books.add(new Book("Diario de Greg. Un renacuajo", "Jeff Kinney", "Novela de ficción-Comedia/Humor", 4));
+        books.add(new Book("Habitos Atómicos", "James Clear", "Libro de autoayuda", 5));
+        books.add(new Book("El Principito", "Antoine de Saint-Exupéry", "Fábula infantil de ficción y con sentido filosófico", 4));
+        books.add(new Book("1984","George Orwell", "Novela política de ficción distópica", 2 ));
+        books.add(new Book("El conde de Montecristo", "Alejandro Dumas y Auguste Maquet", "Novela histórica de ficción con aventura, romance y temas sociales", 3));
+        books.add(new Book("Los tres mosqueteros", "Alexandre Dumas", "Novela literaria de aventura, capa y espada y ficción", 5));
+    }
+
+    public void registerBook(Book book) {
+        books.add(book);
+        System.out.println("Libro registrado: " + book.getTitle());
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void displayBooks(){
+        if(books.isEmpty()) {
+            System.out.println("No existen libros en el registro.");
+        } else {
+            System.out.println("\n--- Lista de Libros ---");
+            for (Book book : books) {
+                System.out.println(book);
+            }
+        }
+    }
+    
+    public Book findBookByTitle(String title) {
+        for (Book book : books) {
+            if (book.getTitle().equalsIgnoreCase(title)) {
+                return book;
+            }
+        }
+
+        System.out.println("No se encuentra libro.");
+        return null;
+    }
 }
+
+
