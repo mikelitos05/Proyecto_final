ba90741760cfda5a57b74f7ba787d401b4a1f792
mikeess05 <miguelmendoza.05.mm@gmail.com>
2025-02-07 18:19:28 -0600

    Hice correcciones en los lenguajes ya que estaban al revez algunos

--- proyectofinal/src/main/java/edu/proyectofinal/ui/En.java
+++ proyectofinal/src/main/java/edu/proyectofinal/ui/En.java
@@ -4,21 +4,22 @@ public class En extends Language {
     public En() {
         WELCOME = "Welcome to the library";
         SELECT_OPTION = "Select an option";
-        OPTION_1 = "1. Add a book";
-        OPTION_2 = "2. Loan a book";
-        OPTION_3 = "3. Return a book";
-        OPTION_4 = "4. Show books";
+        OPTION_1 = "1. Show books";
+        OPTION_2 = "2. Show active users";
+        OPTION_3 = "3. Loan a book";
+        OPTION_4 = "4. Add a book";
         OPTION_5 = "5. Exit";
         INVALID_OPTION = "Invalid option";
-        IMAGINE_BOOKS = "Imagine all the books";
+        IMAGINE_BOOKS = "Imagine all the books you can read";
         ACTIVE_USERS = "Active users";
-        ENTER_USER_NAME = "Enter user name";
-        ENTER_BOOK_LOAN = "Enter book to loan";
+        ENTER_USER_NAME = "Enter the user name";
+        ENTER_BOOK_LOAN = "Enter the book to loan";
         BOOK_LOANED = "Book loaned";
-        ENTER_BOOK_NAME = "Enter book name";
-        ENTER_AUTHOR_NAME = "Enter author name";
-        ENTER_BOOK_YEAR = "Enter book year";
+        ENTER_BOOK_NAME = "Enter the book name";
+        ENTER_AUTHOR_NAME = "Enter the author name";
+        ENTER_BOOK_YEAR = "Enter the book year";
         BOOK_ADDED = "Book added";
         GOODBYE = "Goodbye";
+
     }
 }
--- proyectofinal/src/main/java/edu/proyectofinal/ui/Pr.java
+++ proyectofinal/src/main/java/edu/proyectofinal/ui/Pr.java
@@ -3,22 +3,23 @@ package edu.proyectofinal.ui;
 public class Pr extends Language {
     public Pr() {
         WELCOME = "Bem-vindo à biblioteca";
-        SELECT_OPTION = "Selecione uma opção:";
-        OPTION_1 = "1. Mostrar livros disponíveis";
-        OPTION_2 = "2. Mostrar usuários/empréstimos ativos";
-        OPTION_3 = "3. Adicionar empréstimo";
-        OPTION_4 = "4. Adicionar um livro";
+        SELECT_OPTION = "Selecione uma opção";
+        OPTION_1 = "1. Ver livros";
+        OPTION_2 = "2. Ver usuários ativos";
+        OPTION_3 = "3. Emprestar livro";
+        OPTION_4 = "4. Adicionar livro";
         OPTION_5 = "5. Sair";
-        INVALID_OPTION = "Opção inválida, escolha entre 1 e 5.";
-        IMAGINE_BOOKS = "Imagine que há livros disponíveis aqui:";
-        ACTIVE_USERS = "Usuários ativos: 0";
+        INVALID_OPTION = "Opção inválida";
+        IMAGINE_BOOKS = "Imagine todos os livros que você pode ler";
+        ACTIVE_USERS = "Usuários ativos";
         ENTER_USER_NAME = "Digite o nome do usuário";
-        ENTER_BOOK_LOAN = "Digite o livro que deseja emprestar";
-        BOOK_LOANED = "O livro {0} foi emprestado para {1}";
+        ENTER_BOOK_LOAN = "Digite o livro para emprestar";
+        BOOK_LOANED = "Livro emprestado";
         ENTER_BOOK_NAME = "Digite o nome do livro";
         ENTER_AUTHOR_NAME = "Digite o nome do autor";
         ENTER_BOOK_YEAR = "Digite o ano do livro";
-        BOOK_ADDED = "Livro adicionado: {0}";
+        BOOK_ADDED = "Livro adicionado";
         GOODBYE = "Adeus";
+
     }
 }
